864947f8d33c4fce96750415e52bfe83b6f3ab18
teddy.ma <teddy.ma@ef.com>
2016-09-27 22:33:13 +0800

    fix build script

--- src/builld.ps1
+++ src/builld.ps1
@@ -1,4 +1,4 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild NanoProfiler2.sln /p:Configuration=Release
+& 'C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe' NanoProfiler2.sln /p:Configuration=Release
 
 .\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe .\Tests\NanoProfiler.Tests\bin\Release\NanoProfiler.Tests.dll
 .\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe /cleanup
