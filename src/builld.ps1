8f0f62f5edeae902dd2dd1a727b264b722d9e5f3
teddy.ma <teddy.ma@ef.com>
2020-01-06 16:59:53 +0800

    refactor to use PackageReference instead of package.config

--- src/builld.ps1
+++ src/builld.ps1
@@ -1,10 +1,10 @@
-& 'C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe' NanoProfiler2.Net45.sln /p:Configuration=Release
+& 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe' NanoProfiler2.Net45.sln /p:Configuration=Release
 Remove-Item .\NanoProfiler.Data\bin\net45 -recurse -force
 Copy-Item .\NanoProfiler.Data\bin\Release .\NanoProfiler.Data\bin\net45 -recurse
 
-& 'C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe' NanoProfiler2.sln /p:Configuration=Release
+& 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe' NanoProfiler2.sln /p:Configuration=Release
 Remove-Item .\NanoProfiler.Data\bin\net40 -recurse -force
 Copy-Item .\NanoProfiler.Data\bin\Release .\NanoProfiler.Data\bin\net40 -recurse
 
-.\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe .\Tests\NanoProfiler.Tests\bin\Release\NanoProfiler.Tests.dll
+.\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe .\Tests\NanoProfiler.Tests\bin\Release\NanoProfiler.Tests.dll
 
